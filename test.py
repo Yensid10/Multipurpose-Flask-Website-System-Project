30b6a745120c559e56861f258fcf5646b371576f
Benjamin Shearlock <ben.shearlock@gmail.com>
2023-01-17 01:42:11 +0000

    Changes to Menu & Test
    Just commenting out specific parts and changing things around to allow us to test the programs functionality with arrays

--- test.py
+++ test.py
@@ -1,28 +1,29 @@
 from flask import Flask, render_template
-import mysql.connector
+# import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/menu/<query>')
-def menu(query):
-    # Connect to the database
-    conn = mysql.connector.connect(user='username',
-                                   password='password',
-                                   host='host',
-                                   database='database')
-    cursor = conn.cursor()
+def menu(array):
+# def menu(query):
+    # # Connect to the database
+    # conn = mysql.connector.connect(user='username',
+    #                                password='password',
+    #                                host='host',
+    #                                database='database')
+    # cursor = conn.cursor()
 
-    # Execute the SQL query
-    cursor.execute(query)
+    # # Execute the SQL query
+    # cursor.execute(query)
 
-    # Fetch the menu items
-    menu_items = cursor.fetchall()
+    # # Fetch the menu items
+    # menu_items = cursor.fetchall()
 
-    # Close the cursor and connection
-    cursor.close()
-    conn.close()
+    # # Close the cursor and connection
+    # cursor.close()
+    # conn.close()
 
-    # Render the template with the menu items
-    return render_template('dynamicMenu.html', menu_items=menu_items)
+    # # Render the template with the menu items
+    return render_template('dynamicMenu.html', menu_items=array)
 if name == 'main':
     app.run(debug=True)
