2f42dcae203ab47d7a9a7cc096478f8f25f18396
Musfik <94970638+zjac170@users.noreply.github.com>
2023-01-17 00:53:05 +0000

    Test-file with initial contents for the dynamic menu

---
+++ test.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/menu/<query>')
+def menu(query):
+    # Connect to the database
+    conn = mysql.connector.connect(user='username',
+                                   password='password',
+                                   host='host',
+                                   database='database')
+    cursor = conn.cursor()
+
+    # Execute the SQL query
+    cursor.execute(query)
+
+    # Fetch the menu items
+    menu_items = cursor.fetchall()
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+    # Render the template with the menu items
+    return render_template('dynamicMenu.html', menu_items=menu_items)
+if name == 'main':
+    app.run(debug=True)
