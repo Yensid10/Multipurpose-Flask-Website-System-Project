cfdb923cc20fabd406173af9ed404ad75967e5ea
Benjamin Shearlock <ben.shearlock@gmail.com>
2023-01-19 16:10:55 +0000

    Formatting and Making HTML more menu-like
    I've added formatting to be applied on the project through vscode adhering to pep8 style, I also changed the dynamicMenu so that it's displayed in a more visually understandable way rather then just a table

--- test.py
+++ test.py
@@ -7,32 +7,33 @@ app = Flask(__name__)
 # def home():
 #     return "Hello, Flask!"
 
+
 @app.route("/")
 # @app.route("/menu/<array>")
-def menu(array = [["beans", 12, "Bean Allergy", 123, "this is the quantity, the item is nice"], ["cheese", 11, "cheese Allergy"], ["soup", 10, "soup Allergy"]]):
-# # def menu(query):
-#     # # Connect to the database
-#     # conn = mysql.connector.connect(user='username',
-#     #                                password='password',
-#     #                                host='host',
-#     #                                database='database')
-#     # cursor = conn.cursor()
+# Format will be Name, price, Description, Calories and Allergic/Religious/Dietary requirements.
+def menu(array=[["Beans", 12, "Some Cool Beans", 123, "Bean Allergy?"], ["Cheese", 12, "Some Cool Cheese", 1234, "Chesse Allergy?"], ["Soup", 12, "Some Cool Soup", 321, "Soup Allergy?"]]):
+    # # def menu(query):
+    #     # # Connect to the database
+    #     # conn = mysql.connector.connect(user='username',
+    #     #                                password='password',
+    #     #                                host='host',
+    #     #                                database='database')
+    #     # cursor = conn.cursor()
 
-#     # # Execute the SQL query
-#     # cursor.execute(query)
+    #     # # Execute the SQL query
+    #     # cursor.execute(query)
 
-#     # # Fetch the menu items
-#     # menu_items = cursor.fetchall()
+    #     # # Fetch the menu items
+    #     # menu_items = cursor.fetchall()
 
-#     # # Close the cursor and connection
-#     # cursor.close()
-#     # conn.close()
+    #     # # Close the cursor and connection
+    #     # cursor.close()
+    #     # conn.close()
 
-#     # # Render the template with the menu items
+    #     # # Render the template with the menu items
     return render_template('dynamicMenu.html', menu_items=array)
 
 # if name == 'main':
 #     app.run(debug=True)
 
-# export FLASK_APP=test.py 
-# python -m flask run
+# export FLASK_APP=test.py python -m flask run
