6f2c18c178ed871adf04819912f6c9aea0e83662
Ethan <ZKAC185@live.rhul.ac.uk>
2023-01-21 18:08:10 +0000

    Formating files

--- PingWaiter.py
+++ PingWaiter.py
@@ -1,15 +1,18 @@
 from plyer import notification
 
+
 class Notifty:
-    def main(self,str):
+    def main(self, str):
         self.notification(str)
+
     def notification(self, string):
         notification.notify(
-            title = "Test Ping",
-            message = string,
-            app_icon = "Waiter.ico",
-            timeout = 10,
+            title="Test Ping",
+            message=string,
+            app_icon="Waiter.ico",
+            timeout=10,
         )
+
 
 N = Notifty()
 N.main("Calling for Help")
