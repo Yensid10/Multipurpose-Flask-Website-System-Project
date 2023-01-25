b3bee6c5fdb7b5b95319e5830a84f6035f919696
Benjamin Shearlock <ben.shearlock@gmail.com>
2023-01-25 12:10:27 +0000

    Changes to the file so it can run without calling a terminal command
    ^

--- test.py
+++ test.py
@@ -9,33 +9,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
-# @app.route("/menu/<array>")
-# Format will be Name, price, Description, Calories and Allergic/Religious/Dietary requirements.
 def menu(array=[["Beans", 12, "Some Cool Beans", 123, "Bean Allergy?"],
                 ["Cheese", 11, "Some Cool Cheese", 1234, "Chesse Allergy?"],
                 ["Soup", 10, "Some Cool Soup", 321, "Soup Allergy?"]]):
-    # # def menu(query):
-    #     # # Connect to the database
-    #     # conn = mysql.connector.connect(user='username',
-    #     #                                password='password',
-    #     #                                host='host',
-    #     #                                database='database')
-    #     # cursor = conn.cursor()
 
-    #     # # Execute the SQL query
-    #     # cursor.execute(query)
-
-    #     # # Fetch the menu items
-    #     # menu_items = cursor.fetchall()
-
-    #     # # Close the cursor and connection
-    #     # cursor.close()
-    #     # conn.close()
-
-    #     # # Render the template with the menu items
     return render_template('dynamicMenu.html', menu_items=array)
 
-# if name == 'main':
-#     app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # FLASK_APP=test.py flask --debug run
