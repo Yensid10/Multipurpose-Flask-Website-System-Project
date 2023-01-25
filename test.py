e0f7be5f2720b8747b6512760f372c1856f2594f
zjac170 <zjac170@live.rhul.ac.uk>
2023-01-25 12:02:20 +0000

    Updated the code in order to get the flask app running.

--- test.py
+++ test.py
@@ -35,7 +35,5 @@ def menu(array=[["Beans", 12, "Some Cool Beans", 123, "Bean Allergy?"],
     #     # # Render the template with the menu items
     return render_template('dynamicMenu.html', menu_items=array)
 
-# if name == 'main':
-#     app.run(debug=True)
-
-# FLASK_APP=test.py flask --debug run
+if __name__ == '__main__':
+    app.run(debug=True)
