8a52c84e73267059483727760512dd8cd2ebd2dc
Benjamin Shearlock <ben.shearlock@gmail.com>
2023-02-01 11:59:19 +0000

    Neatened up all of the Python files
    Have deleted unnecessary commented out code, formatted everything neatly and have made sure both files are efficient for their tasks

--- FloorStaffApp.py
+++ FloorStaffApp.py
@@ -1,18 +1,8 @@
 import time
 from ObjectQueue import Queue
 from flask import Flask, jsonify, render_template, request
-from flask import redirect
-from threading import Thread
 
 app = Flask(__name__)
-
-
-# def checkQueue():
-#     while True:
-#         if queue.checkQueue() == "Changed":
-#             return redirect('/')
-#         time.sleep(1)
-
 
 queue = Queue()
 queue.addObject("Food", "#12")
@@ -32,37 +22,18 @@ queue.addObject("Door", "<---")
 
 @app.route('/')
 def home():
-    # thread = Thread(target=checkQueue)
-    # thread.start()
-    # if request.method == 'POST':
-    #     if request.form.get('popQueue') == 'Accept':
-    #         queue.popFrontObject()
-    #         return redirect('/')
     return render_template('Floor-Staff.html', queue=queue)
 
 
-@app.route('/page')
-def page():
-    # thread = Thread(target=checkQueue)
-    # thread.start()
-    # if request.method == 'POST':
-    #     if request.form.get('popQueue') == 'Accept':
-    #         queue.popFrontObject()
-    #         return redirect('/')
-    return render_template('page.html')
-
-
-@app.route('/test', methods=['POST'])
-def test():
+@app.route('/acceptQueuePing', methods=['POST'])
+def acceptQueuePing():
     if request.method == 'POST':
         queue.popFrontObject()
-        # print("popped")
         return ('', 204)
 
 
-@app.route('/addToQueueTest', methods=['POST'])
-def addToQueueTest():
-    print("help")
+@app.route('/addPingToQueue', methods=['POST'])
+def addPingToQueue():
     if request.method == 'POST':
         data = request.get_json()
         tableNo = data.get('tableNo')
@@ -70,31 +41,18 @@ def addToQueueTest():
         return ('', 204)
 
 
-@ app.route('/background_process_test', methods=['GET', 'POST'])
-def background_process_test():
-
-    if request.method == "POST":
-        table_number = request.form.get("TableNum")
-        note = request.form.get("Note")
-        print(table_number)
-        print(note)
-    if table_number and note:
-        queue.addObject(note, table_number)
-    return redirect('/')
-
-
 @app.route("/updateQueue")
 def updateQueue():
-    queueItems = []
+    jsonQueue = []
     for i in range(queue.getLength()):
-        order = queue.getOrder(i)
-        queueItems.append({
+        order = queue.getObject(i)
+        jsonQueue.append({
             "note": order.getNote(),
             "tableNo": order.getTableNo()
         })
     return jsonify({
         "queueLength": queue.getLength(),
-        "queueItems": queueItems
+        "queueItems": jsonQueue
     })
 
 
--- ObjectQueue.py
+++ ObjectQueue.py
@@ -1,17 +1,7 @@
-from threading import Thread
-import threading
-import time
-
-
 class Task:
-    # def __init__(self, TableNo, Note):
-    #     self.TableNo = TableNo
-    #     self.Note = Note
-
     def __init__(self, Note, TableNo):
         self.TableNo = TableNo
         self.Note = Note
-        # self.Price = Price
 
     def getTableNo(self):
         return self.TableNo
@@ -19,62 +9,19 @@ class Task:
     def getNote(self):
         return self.Note
 
-    # def getPrice(self):
-    #     return self.Price
-
 
 class Queue:
     def __init__(self):
         self.queue = []
-        t = threading.Thread(target=self.checkQueue)
-        t.start()
 
     def addObject(self, Note, TableNo):
-        new_object = Task(Note, TableNo)
-        self.queue.append(new_object)
+        self.queue.append(Task(Note, TableNo))
 
     def popFrontObject(self):
-        # if (len(self.queue) == 0):
-        #     return None
-        # task = self.queue[0]
         self.queue.pop(0)
 
-    def getOrder(self, index):
+    def getObject(self, index):
         return self.queue[index]
 
     def getLength(self):
         return len(self.queue)
-
-    def checkQueue(self):
-        size = len(self.queue)
-        while True:
-            current_size = len(self.queue)
-            if (current_size > size or current_size < size):
-                print("Changed")
-                return "Changed"
-            time.sleep(1)
-
-    # def alterRequest(self, TableNum, New_Request):
-    #     Position = -1
-    #     for i, obj in enumerate(self.queue):
-    #         if obj.TableNo == TableNum:
-    #             Position = i
-    #     if (Position >= 0):
-    #         self.queue[Position].Request = New_Request
-
-    # def printQueue(self):
-    #     for order in self.queue:
-    #         print(order.getNote() + " " + order.getTableNo())
-
-    def getQueue(self):
-        return self.queue
-
-# queue = Queue()
-# queue.add_object('14', "Please bring some water")
-# queue.ReadQueue()
-# queue.add_object('15', "Please bring some bread")
-# print(queue.queue[1].Request)
-# queue.AlterRequest('15', "Please bring some water")
-# print(queue.queue[1].Request)
-# p = threading.Thread(target=queue.Check_queue)
-# p.start()
