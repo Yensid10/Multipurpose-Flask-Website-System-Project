d191d504fca0e0205820b743bd514d5e723f449e
Maan M <ZKAC339@live.rhul.ac.uk>
2023-02-01 22:04:41 +0000

    implemented funtionality of accept button, now adds to accepted_orders and removes from order_queue

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
 import sqlite3
+
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
 
 
 @app.route('/kitchen')
@@ -12,6 +13,25 @@ def kitchen():
     orders = c.fetchall()
     conn.close()
     return render_template("kitchen.html", orders=orders)
+
+
+@app.route('/accept_order/<int:order_id>')
+def accept_order(order_id):
+    conn = sqlite3.connect('orders.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM order_queue WHERE id=?", (order_id,))
+    order = cursor.fetchone()
+
+    cursor.execute("INSERT INTO accepted_orders (id, table_number, items, status, recipe_url) VALUES (?, ?, ?, ?, ?)",
+                   (order_id, order[1], order[2], order[3], order[4]))
+    conn.commit()
+
+    cursor.execute("DELETE FROM order_queue WHERE id=?", (order_id,))
+    conn.commit()
+
+    return redirect(url_for('kitchen'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,21 +1,9 @@
-function acceptOrder(orderId) {
-    // Remove the order from the order queue table
-    var orderRow = document.querySelector(`#order-queue-table tbody tr[data-order-id='${orderId}']`);
-    orderRow.remove();
-
-    // Get the order data from the order queue
-    var acceptedOrder = order_queue.find(order => order.id === orderId);
-
-    // Add the order data to the accepted orders table
-    var acceptedOrdersTable = document.querySelector("#accepted-orders-table tbody");
-    var newRow = document.createElement("tr");
-    newRow.innerHTML = `
-        <td>${acceptedOrder.id}</td>
-        <td>${acceptedOrder.customer_name}</td>
-        <td>${acceptedOrder.items}</td>
-        <td>
-            <a href="${acceptedOrder.recipe_url}" class="recipe-link">View Recipe</a>
-        </td>
-    `;
-    acceptedOrdersTable.appendChild(newRow);
-}
+document.addEventListener("DOMContentLoaded", function() {
+document.querySelectorAll('.accept-button').forEach(button => {
+button.addEventListener('click', function () {
+const orderID = this.id.split('-')[2];
+console.log("Accepting order with ID:", orderID);
+window.location.href = '/accept_order/' + orderID;
+});
+});
+});
