3af420ac07c9068c0f1467b12c0086867c942898
Maan M <ZKAC339@live.rhul.ac.uk>
2023-02-05 04:53:04 +0000

    added order history page and tables to keep a log of completed orders, displays on separate page

--- static/script.js
+++ static/script.js
@@ -7,3 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.complete-button').forEach(button => {
+        button.addEventListener('click', function () {
+            const orderID = this.id.split('-')[2];
+            console.log("Completing order with ID:", orderID);
+            window.location.href = '/complete_order/' + orderID;
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.order-history-button').forEach(button => {
+        button.addEventListener('click', function () {
+            const orderID = this.id.split('-')[2];
+            console.log("Redirecting to order history:", orderID);
+            window.location.href = '/order_history';
+        });
+    });
+});
