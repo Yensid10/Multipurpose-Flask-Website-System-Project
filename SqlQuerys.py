c76bb657b1918d7be0abb2dc9056c934e0e8d547
Theamzingu <Socratislioupis@gmail.com>
2023-02-09 22:21:08 +0000

    changed queries from Mysql to mongoDB format

--- SqlQuerys.py
+++ SqlQuerys.py
@@ -1,68 +1,29 @@
-import sqlite3
-from sqlite3 import Error
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+client = MongoClient(
+    'mongodb+srv://Theamzingu:Socr%40tis123@teamproject14.nnzfaib.mongodb.net/test')
+db = client.Menu
 
 
-class queries:
-    def main(self):
-        self.FetchMainAllergiesCalories("Guacamole and chips")
-        self.FetchMenu("Guacamole and chips")
-        self.FetchDescrition("Guacamole and chips")
-
-    def DatabaseConnection(self):
-        conn = None
-        try:
-            conn = sqlite3.connect("Menu.db")
-        except Error as e:
-            print(e)
-
-        return conn
-
-    def FetchMainAllergiesCalories(self, Input):
-        conn = self.DatabaseConnection()
-        a = (conn.execute(
-            "SELECT Calories FROM MENU,Allergies WHERE MENU.Name = '" + Input + "' LIMIT 1"))
-        b = (conn.execute("SELECT Name, "
-                          + "(CASE WHEN gluten = TRUE THEN 'gluten ' ELSE '' END) || "
-                          + "(CASE WHEN peanuts = TRUE THEN 'peanuts ' ELSE '' END) || "
-                          + "(CASE WHEN treenuts = TRUE THEN 'treenuts ' ELSE '' END) || "
-                          + "(CASE WHEN celery = TRUE THEN 'celery ' ELSE '' END) || "
-                          + "(CASE WHEN mustard = TRUE THEN 'mustard ' ELSE '' END) || "
-                          + "(CASE WHEN eggs = TRUE THEN 'eggs ' ELSE '' END) || "
-                          + "(CASE WHEN milk = TRUE THEN 'milk ' ELSE '' END) || "
-                          + "(CASE WHEN sesame = TRUE THEN 'sesame ' ELSE '' END) || "
-                          + "(CASE WHEN fish = TRUE THEN 'fish ' ELSE '' END) || "
-                          + "(CASE WHEN crustaceans = TRUE THEN 'crustaceans ' ELSE '' END) || "
-                          + "(CASE WHEN molluscs = TRUE THEN 'molluscs ' ELSE '' END) || "
-                          + "(CASE WHEN sulphites = TRUE THEN 'sulphites ' ELSE '' END) || "
-                          + "(CASE WHEN lupin = TRUE THEN 'lupin ' ELSE '' END) "
-                          + "FROM Allergies "
-                          + "WHERE Name = '" + Input + "';"))
-        rows = a.fetchall()
-        results = b.fetchall()
-        for row in rows:
-            print(row[0])
-        for result in results:
-            print(result)
-        conn.commit()
-
-    def FetchDescrition(self, Input):
-        conn = self.DatabaseConnection()
-        a = (conn.execute("SELECT Name, Description "
-                          + "FROM Menu "
-                          + "WHERE Name = '" + Input + "';"))
-        results = a.fetchall()
-        for result in results:
-            print(result)
-
-    def FetchMenu(self, Input):
-        conn = self.DatabaseConnection()
-        a = (conn.execute("SELECT Name, price "
-                          + "FROM Menu "
-                          + "WHERE Name = '" + Input + "';"))
-        results = a.fetchall()
-        for result in results:
-            print(result)
+def FetchDescription():
+    menuItem = db.Menu.find_one({"name": "Guacamole and chips"})
+    print(menuItem["name"])
+    print(menuItem["description"])
 
 
-q = queries()
-q.main()
+def FetchMenu():
+    for menuItem in db.Menu.find({}):
+        print(menuItem)
+
+
+def FetchAllergies():
+    for menuItem in db.Menu.find({}):
+        oid = ObjectId(menuItem["_id"])
+        allergies = db.Allergies.find_one({"_id": oid})
+        print(allergies)
+
+
+FetchDescription()
+FetchMenu()
+FetchAllergies()
