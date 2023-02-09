a4607a8deffe073850fbce66c70f8c6bee195318
Ethan <ZKAC185@live.rhul.ac.uk>
2023-02-09 12:52:49 +0000

    Implemented Kitchen ordering system into pages
    Linked another page into flask application

---
+++ Application.py
@@ -0,0 +1,167 @@
+from flask import Flask, render_template, redirect, url_for
+import sqlite3
+from flask import Flask, jsonify, render_template, request
+from flask import redirect
+from ObjectQueue import Queue, Orders
+
+
+app = Flask(__name__)
+
+queue = Queue()
+queue.addObject("Food", "#12")
+queue.addObject("Table", "#3")
+queue.addObject("Table", "#17")
+queue.addObject("Door", "<---")
+queue.addObject("Food", "#12")
+queue.addObject("Table", "#3")
+queue.addObject("Table", "#17")
+queue.addObject("Door", "<---")
+queue.addObject("Food", "#12")
+queue.addObject("Table", "#3")
+queue.addObject("Table", "#17")
+queue.addObject("Door", "<---")
+# Colour coordinate these depending on the type of request?
+
+# Testing Orders queue implementation
+order = Orders()
+order.addOrder("Guacamole and chips", "this is a test")
+print(order)
+
+
+@app.route('/')
+def home():
+    return render_template('menu.html')
+
+
+# @app.route('/')
+# def home():
+#     return render_template('Floor-Staff.html', queue=queue)
+
+@app.route('/acceptQueuePing', methods=['POST'])
+def acceptQueuePing():
+    if request.method == 'POST':
+        ping = queue.popFrontObject()
+        data = {
+            "acceptedPing": ping.getNote() + " " + ping.getTableNo(),
+        }
+        return jsonify(data)
+
+
+@app.route('/addPingToQueue', methods=['POST'])
+def addPingToQueue():
+    if request.method == 'POST':
+        data = request.get_json()
+        tableNo = data.get('tableNo')
+        queue.addObject("Table ", tableNo)
+        return ('', 204)
+
+
+@app.route("/updateQueue")
+def updateQueue():
+    jsonQueue = []
+    for i in range(queue.getLength()):
+        order = queue.getObject(i)
+        jsonQueue.append({
+            "note": order.getNote(),
+            "tableNo": order.getTableNo()
+        })
+    return jsonify({
+        "queueLength": queue.getLength(),
+        "queueItems": jsonQueue
+    })
+
+
+@app.route('/background_process_test', methods=['GET', 'POST'])
+def background_process_test():
+    if request.method == "POST":
+        table_number = request.form.get("TableNum")
+        Request = request.form.get("Request")
+        print(table_number)
+        print(Request)
+    if table_number and Request:
+        Queue().add_object(table_number, Request)
+    return redirect('/')
+
+
+@app.route('/Ring')
+def showRing():
+    return render_template('Ring.html')
+
+
+@app.route('/Floor-Staff')
+def showFS():
+    return render_template('Floor-Staff.html', queue=queue)
+
+
+@app.route('/kitchen')
+def kitchen():
+    conn = sqlite3.connect("orders.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM order_queue")
+    orders = c.fetchall()
+    c.execute("SELECT * FROM accepted_orders")
+    accepted_orders = c.fetchall()
+    conn.close()
+    return render_template("kitchen.html", orders=orders, accepted_orders=accepted_orders)
+
+
+@app.route('/order_history')
+def order_history():
+    conn = sqlite3.connect("orders.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM order_history")
+    order_history = c.fetchall()
+    conn.close()
+    return render_template('order_history.html', order_history=order_history)
+
+
+@app.route('/accept_order/<int:order_id>')
+def accept_order(order_id):
+    conn = sqlite3.connect('orders.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM order_queue WHERE id=?", (order_id,))
+    order = cursor.fetchone()
+
+    cursor.execute("INSERT INTO accepted_orders (id, table_number, items, status, recipe_url) VALUES (?, ?, ?, ?, ?)",
+                   (order_id, order[1], order[2], order[3], order[4]))
+    conn.commit()
+
+    cursor.execute("DELETE FROM order_queue WHERE id=?", (order_id,))
+    conn.commit()
+
+    conn = sqlite3.connect('orders.db')
+    c = conn.cursor()
+    c.execute(
+        "UPDATE accepted_orders SET status = '<preparing>' WHERE id=?", (order_id,))
+    conn.commit()
+
+    return redirect(url_for('kitchen'))
+
+
+@app.route('/complete_order/<int:order_id>')
+def complete_order(order_id):
+    conn = sqlite3.connect('orders.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM accepted_orders WHERE id=?", (order_id,))
+    order = cursor.fetchone()
+
+    cursor.execute("INSERT INTO order_history (id, table_number, items, status, recipe_url) VALUES (?, ?, ?, ?, ?)",
+                   (order_id, order[1], order[2], order[3], order[4]))
+    conn.commit()
+
+    cursor.execute("DELETE FROM accepted_orders WHERE id=?", (order_id,))
+    conn.commit()
+
+    conn = sqlite3.connect('orders.db')
+    c = conn.cursor()
+    c.execute(
+        "UPDATE order_history SET status = '<complete>' WHERE id=?", (order_id,))
+    conn.commit()
+
+    return redirect(url_for('kitchen'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
