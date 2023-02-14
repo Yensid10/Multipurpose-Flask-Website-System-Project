84631a1f38049eaa3c0e4c43d9c51b64277bd9ca
Maan Meher <zkac339@live.rhul.ac.uk>
2023-02-14 02:14:31 +0000

    retrieve and display data from MongoDB database and collection

--- Application.py
+++ Application.py
@@ -1,96 +1,25 @@
-import sqlite3
-
-from flask import Flask, render_template, redirect, url_for, jsonify
+from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Connect to MongoDB
+client = MongoClient('mongodb+srv://Theamzingu:Socr%40tis123@teamproject14.nnzfaib.mongodb.net/test')
 
-@app.route('/kitchen')
+db = client["Kitchen"]
+collection = db["order_queue"]
+
+
+@app.route("/kitchen")
 def kitchen():
-    conn = sqlite3.connect("orders.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM order_queue")
-    orders = c.fetchall()
-    c.execute("SELECT * FROM accepted_orders")
-    accepted_orders = c.fetchall()
-    conn.close()
-    return render_template("kitchen.html", orders=orders, accepted_orders=accepted_orders)
-
-
-@app.route('/order_history')
-def order_history():
-    conn = sqlite3.connect("orders.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM order_history")
-    order_history = c.fetchall()
-    conn.close()
-    return render_template('order_history.html', order_history=order_history)
-
-
-@app.route('/accept_order/<int:order_id>')
-def accept_order(order_id):
-    conn = sqlite3.connect('orders.db')
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM order_queue WHERE id=?", (order_id,))
-    order = cursor.fetchone()
-
-    cursor.execute("INSERT INTO accepted_orders (id, table_number, items, status, recipe_url) VALUES (?, ?, ?, ?, ?)",
-                   (order_id, order[1], order[2], order[3], order[4]))
-    conn.commit()
-
-    cursor.execute("DELETE FROM order_queue WHERE id=?", (order_id,))
-    conn.commit()
-
-    conn = sqlite3.connect('orders.db')
-    c = conn.cursor()
-    c.execute("UPDATE accepted_orders SET status = '<preparing>' WHERE id=?", (order_id,))
-    conn.commit()
-
-    return redirect(url_for('kitchen'))
-
-
-@app.route('/complete_order/<int:order_id>')
-def complete_order(order_id):
-    conn = sqlite3.connect('orders.db')
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM accepted_orders WHERE id=?", (order_id,))
-    order = cursor.fetchone()
-
-    cursor.execute("INSERT INTO order_history (id, table_number, items, status, recipe_url) VALUES (?, ?, ?, ?, ?)",
-                   (order_id, order[1], order[2], order[3], order[4]))
-    conn.commit()
-
-    cursor.execute("DELETE FROM accepted_orders WHERE id=?", (order_id,))
-    conn.commit()
-
-    conn = sqlite3.connect('orders.db')
-    c = conn.cursor()
-    c.execute("UPDATE order_history SET status = '<complete>' WHERE id=?", (order_id,))
-    conn.commit()
-
-    return redirect(url_for('kitchen'))
+    orders = list(collection.find())
+    return render_template("kitchen.html", orders=orders)
 
 
 @app.route("/order_queue_data")
 def order_queue_data():
-    conn = sqlite3.connect("orders.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM order_queue")
-    order_queue = c.fetchall()
-    conn.close()
-    return jsonify(order_queue)
-
-
-@app.route("/accepted_orders_data")
-def accepted_orders_data():
-    conn = sqlite3.connect("orders.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM accepted_orders")
-    accepted_orders = c.fetchall()
-    conn.close()
-    return jsonify(accepted_orders)
+    orders = list(collection.find({}, {"_id": False}))
+    return jsonify(orders)
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -1,32 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('.accept-button').forEach(button => {
-        button.addEventListener('click', function () {
-            const orderID = this.id.split('-')[2];
-            console.log("Accepting order with ID:", orderID);
-            window.location.href = '/accept_order/' + orderID;
-        });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('.complete-button').forEach(button => {
-        button.addEventListener('click', function () {
-            const orderID = this.id.split('-')[2];
-            console.log("Completing order with ID:", orderID);
-            window.location.href = '/complete_order/' + orderID;
-        });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('.order-history-button').forEach(button => {
-        button.addEventListener('click', function () {
-            const orderID = this.id.split('-')[2];
-            console.log("Redirecting to order history:", orderID);
-            window.location.href = '/order_history';
-        });
-    });
-});
 
 var oqprevData;
 
@@ -47,26 +18,3 @@ document.addEventListener("DOMContentLoaded", function () {
 }
 setInterval(getOrderQueueData, 5000);
 });
-
-
-
-var aoprevData;
-
-document.addEventListener("DOMContentLoaded", function () {
-    function getAcceptedOrdersData() {
-    $.getJSON("/accepted_orders_data", function (aodata) {
-        if (aoprevData == null) {
-            aoprevData = aodata;
-        }
-
-        else if (JSON.stringify(aodata) !== JSON.stringify(aoprevData)) {
-            console.log("Accepted orders data changed, reloading page...");
-            aoprevData = aodata;
-            location.reload();
-        }
-    });
-}
-setInterval(getAcceptedOrdersData, 5000);
-});
-
-
