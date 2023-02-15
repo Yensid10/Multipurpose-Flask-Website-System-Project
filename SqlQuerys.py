e4d97a971fbf07de0b1f3a2f3989cee8f2fde4c7
Yensid10 <ben.shearlock@gmail.com>
2023-02-15 14:26:37 +0000

    Changed the queries so they return simple array
    I've changed the queries so they return an easy array that can access certain parts of it easy through a dictionary style of calling. This is going to be used within the dynamic menu to display specific bits of information on the page...

--- SqlQuerys.py
+++ SqlQuerys.py
@@ -1,29 +1,19 @@
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
 
 client = MongoClient(
     'mongodb+srv://Theamzingu:Socr%40tis123@teamproject14.nnzfaib.mongodb.net/test')
 db = client.Menu
 
 
-def FetchDescription():
-    menuItem = db.Menu.find_one({"name": "Guacamole and chips"})
-    print(menuItem["name"])
-    print(menuItem["description"])
-
-
 def FetchMenu():
-    for menuItem in db.Menu.find({}):
-        print(menuItem)
+    menuItem = []
+    for item in db.Menu.find({}):
+        menuItem.append(item)
+    return menuItem
 
 
-def FetchAllergies():
-    for menuItem in db.Menu.find({}):
-        oid = ObjectId(menuItem["_id"])
-        allergies = db.Allergies.find_one({"_id": oid})
-        print(allergies)
-
-
-FetchDescription()
-FetchMenu()
-FetchAllergies()
+for item in FetchMenu():
+    print(item["name"])
+    print(item["price"])
+    print(item["description"])
