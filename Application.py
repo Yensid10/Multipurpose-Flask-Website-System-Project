91a71a3f9f0fdb15f9b3dd6fcee52ef68ed2d653
Maan Meher <zkac339@live.rhul.ac.uk>
2023-02-20 02:56:34 +0000

    fixed method so that table is fully scrollable and only aligned to left of page

--- Application.py
+++ Application.py
@@ -5,24 +5,26 @@ app = Flask(__name__)
 
 # Connect to MongoDB
 client = MongoClient('mongodb+srv://Theamzingu:Socr%40tis123@teamproject14.nnzfaib.mongodb.net/test')
-
 db = client["Kitchen"]
-collection = db["order_queue"]
+order_collection = db["order_queue"]
+accepted_collection = db["accepted_orders"]
 
 
 @app.route('/kitchen')
 def kitchen():
-    orders = list(collection.find({}, {'_id': False}))
+    orders = list(order_collection.find({}, {'_id': False}))
     for order in orders:
         order['items'] = ', '.join(order['items'])
     return render_template('kitchen.html', orders=orders)
 
+
 @app.route('/order_queue_data')
 def order_queue_data():
-    orders = list(collection.find({}, {'_id': False}))
+    orders = list(order_collection.find({}, {'_id': False}))
     for order in orders:
         order['items'] = ', '.join(order['items'])
     return jsonify(orders)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
