7c8bdfef7dfdcd0bd26254afba0b9c7e339b4276
Yensid10 <ben.shearlock@gmail.com>
2023-03-19 18:31:56 +0000

    Moved javascript from Floor-Staff.html and added jsdoc
    I've moved the javascript that was within the Floor-Staff.html page and I put it into its own file, then I generated a JSdoc to test its functionality, now I know how to document the rest of the code

---
+++ static/js/floorStaff.js
@@ -0,0 +1,207 @@
+
+// CLIENT CLASSES \\
+/**
+ * @class orderNotes
+ * @classdesc This class is used to store the order notes and price of an order
+ * @param {string} Note1 - The first note of the order
+ * @param {string} Note2 - The second note of the order
+ * @param {number} price - The price of the order
+ */
+class orderNotes {
+    constructor(Note1, Note2, price) {
+        this.Note1 = Note1;
+        this.Note2 = Note2;
+        this.price = price;
+    }
+
+    // Getters
+    getNote1() {
+        return this.Note1;
+    }
+
+    getNote2() {
+        return this.Note2;
+    }
+
+    getPrice() {
+        return this.price;
+    }
+}
+
+class orderQueue {
+    constructor() {
+        this.queue = [];
+    }
+
+    addObject(Note1, Note2, price) {
+        // Add a new orderNotes object to the queue array
+        this.queue.push(new orderNotes(Note1, Note2, price));
+    }
+
+    removeObject(index) {
+        // Remove an object from the queue array at the specified index
+        this.queue.splice(index, 1);
+    }
+
+    getObject(index) {
+        // Get the object at the specified index of the queue array
+        return this.queue[index];
+    }
+
+    getLength() {
+        // Get the length of the queue array
+        return this.queue.length;
+    }
+}
+
+// QUEUE FUNCTIONS \\
+function acceptQueuePing() {
+    // If the info-container div contains "Complete!" or "htmlCheck", change info-container to be the top popped ping from the python ping queue
+    if (($("#info-container").html().includes("Complete!")) || ($("#info-container").html().includes("htmlCheck"))) {
+        $.ajax({
+            type: "POST",
+            url: "/acceptQueuePing",
+            success: function (data) {
+                $("#info-container").html(data.acceptedPing);
+            }
+        })
+        updateQueue();
+    }
+}
+
+function completeCurrentJob() {
+    // If the info-container div doesn't contain "htmlCheck" or "Complete!", add "Complete!" and a flash animation to the div
+    if ((!$("#info-container").html().includes("htmlCheck"))
+        && (!$("#info-container").html().includes("Complete!"))) {
+        $("#info-container").html("<span id='fade-out-text' class='fade-out-text'>Complete!</span>");
+        setTimeout(function () { $("#fade-out-text").css("opacity", "0"); }, 50);
+    }
+}
+
+function updateQueue() {
+    // Send a GET request to the server to get the ping queue data and update the page accordingly
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/updateQueue", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Parse the response from the server
+            var data = JSON.parse(xhr.responseText);
+            // Update the queue count on the page
+            document.querySelector("h2").innerHTML = "QUEUE: " + data.queueLength;
+            // Clear the existing queue
+            var menu = document.querySelector(".scroll-menu");
+            menu.innerHTML = "";
+
+            // Adding the first new queue objects to the page with button
+            var item = document.createElement("div");
+            item.classList.add("menu-item");
+            item.innerHTML = '<button type="button" class="button" onclick="acceptQueuePing()">Accept</button>'
+                + data.queueItems[0].note + " " + data.queueItems[0].tableNo;
+            menu.appendChild(item);
+
+            // Adding the rest of the new queue objects to the page
+            for (var i = 1; i < data.queueItems.length; i++) {
+                item = document.createElement("div");
+                item.classList.add("menu-item");
+                item.innerHTML = data.queueItems[i].note + " " + data.queueItems[i].tableNo;
+                menu.appendChild(item);
+            }
+        }
+    };
+    xhr.send();
+}
+
+
+// ORDER FUNCTIONS \\        
+function flashElement(element) {
+    // Creates an animation effect called "flash" on an element which will flash red and then fade away
+    if (element.classList.contains("flash")) {
+        element.classList.remove("flash");
+        void element.offsetWidth;
+        element.classList.add("flash");
+    } else {
+        element.classList.add("flash");
+    }
+}
+
+function updateOrder() {
+    // Updates the order review list on the webpage and displays the total price of the order
+    var orderHtml = "";
+    priceTotal = 0;
+    if (order.getLength() > 0) {
+        for (var i = 0; i < order.getLength(); i++) {
+            orderHtml += "<div><strong>" + order.getObject(i).getNote1() + "</strong><br>"
+                + order.getObject(i).getNote2()
+                + "<button class=\"remove-button\" onclick=\"removeOrderItem(" + i + ")\"><img src=\"/static/images/trashcan.png\" alt=\"Remove\"></button></div><br>";
+            priceTotal += parseFloat(order.getObject(i).getPrice());
+        }
+        $("#order-item").html(orderHtml + "<strong style='text-decoration: underline; '>Total: £" + priceTotal + "</strong><br><br>");
+    } else {
+        $("#order-item").html("");
+    }
+}
+
+
+function addItemToOrder(orderItem, price) {
+    // Adds an item to the order list and updates the display
+    document.getElementById("review").classList.remove("flash");
+    order.addObject(orderItem, document.getElementById(orderItem).value, price);
+    document.getElementById(orderItem).value = "";
+    updateOrder();
+}
+
+function removeOrderItem(index) {
+    // Removes an item from the order list and updates the display
+    order.removeObject(index);
+    updateOrder();
+}
+
+function sendToKitchen() {
+    // Sends the order to the kitchen via an AJAX call and updates the display accordingly
+    var selectElement = document.getElementById("orderSelect");
+    if (selectElement.options[selectElement.selectedIndex].value.includes("htmlCheck")) {
+        flashElement(selectElement)
+    }
+    if (order.getLength() == 0) {
+        flashElement(document.getElementById("review"));
+    }
+    if (!selectElement.options[selectElement.selectedIndex].value.includes("htmlCheck") && order.getLength() != 0) {
+        $.ajax({
+            type: "POST",
+            url: "/sendToKitchen",
+            data: JSON.stringify({ "order": order, "tableNo": selectElement.options[selectElement.selectedIndex].value }),
+            contentType: "application/json",
+        });
+        order = new orderQueue();
+        updateOrder();
+        $("#order-item").html("<span id='fade-out-text' style='text-align: center; font-size: 25px; font-weight: bold;"
+            + "color: red; text-decoration: underline;' class='fade-out-text'>Sent to Kitchen!</span>");
+        setTimeout(function () { $("#fade-out-text").css("opacity", "0"); }, 50);
+        selectElement.selectedIndex = 0;
+    }
+}
+
+// BILLS FUNCTIONS \        
+function getBill() {
+    // Retrieves the bill for a particular table and redirects the user to that specific tables bill page
+    var selectElement = document.getElementById("billSelect");
+    if (!selectElement.options[selectElement.selectedIndex].value.includes("htmlCheck")) {
+        var form = document.createElement('form');
+        form.action = '/getBill'; // Set the form action URL
+        form.method = 'POST'; // Set the form submission method
+        var input = document.createElement('input');
+        input.name = 'tableNo';
+        input.value = selectElement.options[selectElement.selectedIndex].value;
+        form.appendChild(input);
+        document.body.appendChild(form);
+        form.submit();
+    } else {
+        flashElement(selectElement);
+    }
+}
+
+
+// MAIN JAVASCRIPT CODE \\
+order = new orderQueue();
+updateQueue();
+setInterval(updateQueue, 1000); 
